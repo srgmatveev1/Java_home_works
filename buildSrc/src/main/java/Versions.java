740f4418ad67cd3f1086333f5476c61045d8430c
sergio <srgmatveev@gmail.com>
2021-09-28 18:55:50 +0300

    first home work commit

---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,26 @@
+public interface Versions {
+    String guava = "28.2-jre";
+    String jmh = "1.23";
+    String jol = "0.10";
+    String asm = "8.0.1";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.12.1";
+    String jaxbApi = "2.3.1";
+    String testcontainers = "1.13.0";
+    String postgresql = "42.2.5";
+    String lombok = "1.18.12";
+    String gson = "2.8.6";
+    String mongodb = "3.12.4";
+    String mongodbReactive = "1.13.0";
+    String cassandra = "3.8.0";
+    String neo4j = "4.0.0";
+    String jedis = "3.2.0";
+    String jetty = "9.4.27.v20200227";
+    String freemarker = "2.3.28";
+    String reflections = "0.9.12";
+    String apacheCommons = "3.9";
+    String sockjs = "1.1.2";
+    String stomp = "2.3.3-1";
+    String bootstrap = "4.4.1-1";
+    String netty = "4.1.45.Final";
+}
